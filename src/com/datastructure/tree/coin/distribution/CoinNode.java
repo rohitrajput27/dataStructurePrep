4624b79e2a5c73709266a1e1f6417bc4d99cded2
Rohit Rajput <rohit.rajput27@gmail.com>
2018-10-28 22:38:03 -0500

    initial commit

---
+++ src/com/datastructure/tree/coin/distribution/CoinNode.java
@@ -0,0 +1,22 @@
+package com.datastructure.tree.coin.distribution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoinNode {
+    private List<CoinNode> coinNodes = new ArrayList<>();
+    private Integer coin;
+
+    public Integer getCoin() {
+        return coin;
+    }
+
+    public void setCoin(Integer coin) {
+        this.coin = coin;
+    }
+
+    public List<CoinNode> getCoinNodes() {
+        return coinNodes;
+    }
+
+}
