34c7cd15ea475f8fb5a4d25689d6769febc5df62
Rohit Rajput <rohit.rajput27@gmail.com>
2018-11-01 11:18:11 -0500

    initial commit

---
+++ src/com/algo/maxlenghtin/array/MaxLengthInArray.java
@@ -0,0 +1,22 @@
+package com.algo.maxlenghtin.array;
+
+public class MaxLengthInArray {
+
+    public static void main(String[] args) {
+        int length = -1;
+        int[] data = {13, 2, 3, 2, 1, 10, 12};
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = i + 1; j < data.length; j++) {
+
+                if (data[i] < data[j]) {
+                    System.out.println("" + i + ":" + data[i] + "  " + j + ":" + data[j] + "  " + (j - i));
+                    if (length < j - i) {
+                        System.out.println("final : " + i + ":" + data[i] + "  " + j + ":" + data[j] + "  " + (j - i));
+                        length = j - i;
+                    }
+                }
+            }
+        }
+        System.out.println(length);
+    }
+}
--- src/com/datastructure/tree/coin/distribution/CoinDistribution.java
+++ src/com/datastructure/tree/coin/distribution/CoinDistribution.java
@@ -55,6 +55,6 @@ public class CoinDistribution {
     }
 
     private void distributeCoin() {
-        System.out.println("test");
+
     }
 }
